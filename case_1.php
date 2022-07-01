b7dddcce191f596a197127bcc6252170a1dffe67
soneoso <eli.dekeersgieter@gmail.com>
2022-07-01 11:26:55 +0200

    added class, function construct, products

--- case_1.php
+++ case_1.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+// =================================================================================================
+//                                     ARRAY CALCULATION 
+// =================================================================================================
+
+
 $products = (array)[
     ['name'=>'Banana', 'amount'=>6, 'price'=>1, 'tax'=>6],
     ['name'=>'Apple', 'amount'=>3, 'price'=>1.5, 'tax'=>6],
@@ -24,11 +29,54 @@ foreach($products as $i => $product) {
     $cartTotal += $product['amount'];
 }
 
+// =================================================================================================
+//                                      CLASS CALCULATION 
+// =================================================================================================
+
+class Products {
+    public string $name;
+    public int $amount;
+    public float $price;
+    public int $tax;
+
+    
+        function __construct($name, $amount, $price, $tax) {
+            $this->name = $name;
+            $this->amount = $amount;
+            $this->price = $price;
+            $this->tax = $tax;
+        }
+}
+    
+
+    $banana = new Products ("banana", 6 , 1,  6);
+    $apple = new Products ("apple",3,1.5, 6 );
+    $wine = new Products (  "wine", 2, 10, 21);
+
+
+    
+
+
 ?>
 
-<h2 id="cart-total-products">Items in cart<br>
+<!-- =================================================================================================
+                                        HTML STRUCTURE
+================================================================================================= -->
+
+<!-- WITH ARRAY -->
+<h1>Calculation with array</h2>
+<h2 id="cart-total-products">Items in cart <br>
     <?php echo $cartTotal ?></h2>
 <h2 id="cart-total-price">Total price of your products <br>
     <?php echo '€',  $priceTotal ?></h2>
 <h2 id="cart-total-price">Total price of your products including vat <br>
     <?php echo '€',  $priceTotalWithTax ?></h2>
+
+    <!-- WITH CLASSES -->
+<h1>Calculation with classes</h1>
+<h2 id="cart-total-products2">Items in cart <br>
+<?php echo $cartTotal ?> </h2>
+<h2 id="cart-total-products2">Total price of your products <br>
+<?php echo $priceTotal ?> </h2>
+<h2 id="cart-total-products2">Total price of your products including vat<br>
+<?php echo $priceTotalWithTax ?> </h2>
